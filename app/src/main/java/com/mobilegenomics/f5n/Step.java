62dc0d1c1ec667aeaeceb0cefb13fae13b6c3732
Sanoj <sanoj.punchihewa@gmail.com>
2019-09-23 16:35:14 +0530

    Update arguments

--- app/src/main/java/com/mobilegenomics/f5n/Step.java
+++ app/src/main/java/com/mobilegenomics/f5n/Step.java
@@ -13,7 +13,7 @@ public class Step {
 
     private StringBuilder command;
 
-    public PipelineStep getStepName() {
+    public PipelineStep getStep() {
         return stepName;
     }
 
@@ -61,55 +61,130 @@ public class Step {
 
     private void minimap2Arguments() {
         Argument argument = new Argument(true, "reference index", null, "Path to the reference index file", false,
-                null);
+                null, false);
         arguments.add(argument);
-        argument = new Argument(true, "query sequence", null, "Path to the query sequence file", false, null);
+        argument = new Argument(true, "query sequence", null, "Path to the query sequence file", false, null, false);
         arguments.add(argument);
-        argument = new Argument(false, "output file", null, "Path to the output sam file", true, "-o");
+        argument = new Argument(true, "output file", null, "Path to the output sam file", true, "-o", false);
+        arguments.add(argument);
+        argument = new Argument(false, "Output format SAM", null,
+                "Generate CIGAR and output alignments in SAM ", true, "-a", true);
+        arguments.add(argument);
+        argument = new Argument(false, "Output format PAF", null,
+                "Generate CIGAR and output alignments in PAF format", true, "-c", true);
+        arguments.add(argument);
+//        argument = new Argument(true, "Preset", "map-ont",
+//                "Preset type (map-ont, map-pb, splice, ...)", true, "-x", false);
+//        arguments.add(argument);
+        argument = new Argument(false, "Threads", "3",
+                "Number of threads [3]", true, "-t", false);
+        arguments.add(argument);
+        argument = new Argument(false, "Bases loaded into memory", "500M",
+                "Number of bases loaded into memory[500M]", true, "-K", false);
         arguments.add(argument);
     }
 
     private void samtoolSortArguments() {
-//        Argument argument = new Argument(true, "input sam", null, "Path to the input index sam file", false, null);
-//        arguments.add(argument);
-//        argument = new Argument(true, "output bam", null, "Path to the output bam file", true, "-o");
-//        arguments.add(argument);
+        Argument argument = new Argument(true, "input sam", null, "Path to the input sam file", false, null, false);
+        arguments.add(argument);
+        argument = new Argument(true, "output file", null, "Path to the output file", true, "-o", false);
+        arguments.add(argument);
+        argument = new Argument(false, "threads", null, "Number of threads[1]", true, "-@", false);
+        arguments.add(argument);
     }
 
-    // TODO
     private void samtoolIndexArguments() {
-//        Argument argument = new Argument("input sam", null, "Path to the input index sam file", false, null);
-//        arguments.add(argument);
-//        argument = new Argument("output bam", null, "Path to the output bam file", true, "-o");
-//        arguments.add(argument);
+        Argument argument = new Argument(true, "input bam", null, "Path to the input bam file", false, null, false);
+        arguments.add(argument);
+        argument = new Argument(false, "threads", null, "Number of threads[1]", true, "-@", false);
+        arguments.add(argument);
     }
 
-    // TODO
     private void f5cIndexArguments() {
-//        Argument argument = new Argument("input sam", null, "Path to the input index sam file", false, null);
-//        arguments.add(argument);
-//        argument = new Argument("output bam", null, "Path to the output bam file", true, "-o");
-//        arguments.add(argument);
+        //Build an index mapping from basecalled reads to the signals measured by the sequencer (same as nanopolish index)
+        Argument argument = new Argument(true, "fast5 files", null,
+                "path to the directory containing the raw ONT signal files. This option can be given multiple times.",
+                true, "--directory", false);
+        arguments.add(argument);
+        argument = new Argument(true, "fast(a or q) reads", null,
+                "Path to the reads.fasta or fastq file", false, null, false);
+        arguments.add(argument);
+        argument = new Argument(false, "summary file", null,
+                "the sequencing summary file from albacore, providing this option will make indexing much faster",
+                true, "--sequencing-summary", false);
+        arguments.add(argument);
+        argument = new Argument(false, "paths to summary files", null,
+                "file containing the paths to the sequencing summary files (one per line)",
+                true, "--summary-fofn", false);
+        arguments.add(argument);
+        argument = new Argument(false, "verbose", null,
+                "display verbose output",
+                true, "--verbose", true);
+        arguments.add(argument);
+
     }
 
-    // TODO
     private void f5cCallMethylationArguments() {
-//        Argument argument = new Argument("input sam", null, "Path to the input index sam file", false, null);
-//        arguments.add(argument);
-//        argument = new Argument("output bam", null, "Path to the output bam file", true, "-o");
+        //Classify nucleotides as methylated or not (optimised nanopolish call-methylation)
+        Argument argument = new Argument(true, "fast5 files", null, "fastq/fasta read file", true, "-r", false);
+        arguments.add(argument);
+        argument = new Argument(true, "sorted bam file", null, "sorted bam file", true, "-b", false);
+        arguments.add(argument);
+        argument = new Argument(true, "reference genome", null, "reference genome", true, "-g", false);
+        arguments.add(argument);
+        argument = new Argument(false, "number of threads", null, "number of threads [8]", true, "-t", false);
+        arguments.add(argument);
+        argument = new Argument(false, "batch size", null,
+                "batch size (max number of reads loaded at once) [512]", true, "-K", false);
+        arguments.add(argument);
+        argument = new Argument(false, "max Bases", null,
+                "max number of bases loaded at once [2.0M]", true, "-B", false);
+        arguments.add(argument);
+        argument = new Argument(false, "help", null,
+                "help", true, "-h", true);
+        arguments.add(argument);
+        argument = new Argument(false, "min map quality", null,
+                "minimum mapping quality [30]", true, "--min-mapq", false);
+        arguments.add(argument);
+        argument = new Argument(false, "consider secondary map", null,
+                "consider secondary mappings or not [no]", true, "--secondary=", false);
+        arguments.add(argument);
+        argument = new Argument(false, "skip unreadable or terminate", null,
+                "skip any unreadable fast5 or terminate program [yes]", true, "--skip-unreadable=", false);
+        arguments.add(argument);
+        argument = new Argument(false, "verbosity", null,
+                "verbosity level [0]", true, "--verbose", false);
+        arguments.add(argument);
+//        argument = new Argument(false, "version", null,
+//                "print version", true, "--version",true);
 //        arguments.add(argument);
     }
 
-    // TODO
     private void f5cEventAlignArguments() {
-//        Argument argument = new Argument("input sam", null, "Path to the input index sam file", false, null);
-//        arguments.add(argument);
-//        argument = new Argument("output bam", null, "Path to the output bam file", true, "-o");
-//        arguments.add(argument);
+
+        //Align nanopore events to reference k-mers (optimised nanopolish eventalign)
+        Argument argument = new Argument(true, "fastq/fasta read file", null,
+                "fastq/fasta read file", true, "-r", false);
+        arguments.add(argument);
+        argument = new Argument(true, "sorted bam", null,
+                "path to sorted bam file", true, "-b", false);
+        arguments.add(argument);
+        argument = new Argument(true, "reference genome", null,
+                "path to reference genome", true, "-g", false);
+        arguments.add(argument);
+        argument = new Argument(false, "threads", "8",
+                "Number of threads[8]", true, "-t", false);
+        arguments.add(argument);
+        argument = new Argument(false, "batch size", "512",
+                "max number of reads loaded at once[512]", true, "-K", false);
+        arguments.add(argument);
+        argument = new Argument(false, "bases loaded at once[K / M / G]", "2.0M",
+                "max number of bases loaded at once [2.0M]", true, "-B", false);
+        arguments.add(argument);
     }
 
     public void buildCommandString() {
-        command = new StringBuilder(stepName.name());
+        command = new StringBuilder(stepName.getCommand());
         for (Argument argument : arguments) {
             command.append(" ");
             command.append(argument.toString());
