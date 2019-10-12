9508b01b472b0808ff3cb6c37a9daae6ccbff683
Sanoj <sanoj.punchihewa@gmail.com>
2019-10-12 20:01:44 +0530

    Update steps with argument ID and linked arguments

--- app/src/main/java/com/mobilegenomics/f5n/Step.java
+++ app/src/main/java/com/mobilegenomics/f5n/Step.java
@@ -1,6 +1,7 @@
 package com.mobilegenomics.f5n;
 
 import android.util.Log;
+import android.util.Pair;
 import java.util.ArrayList;
 
 public class Step {
@@ -60,13 +61,20 @@ public class Step {
     }
 
     private void minimap2Arguments() {
-        Argument argument = new Argument(true, "reference index", null, "Path to the reference index file", false,
-                null, false, true);
+        Argument argument = new Argument(ArgumentID.MINIMAP2_REF_FILE, true, "reference index", null, null,
+                "Path to the reference index file",
+                false, null, false,
+                true, new Pair<>(PipelineStep.F5C_CALL_METHYLATION, ArgumentID.F5C_METH_REF_FILE));
         arguments.add(argument);
-        argument = new Argument(true, "query sequence", null, "Path to the query sequence file", false, null, false,
-                true);
+        argument = new Argument(ArgumentID.MINIMAP2_QUERY_FILE, true, "query sequence", null, null,
+                "Path to the query sequence file",
+                false, null, false,
+                true, new Pair<>(PipelineStep.F5C_INDEX, ArgumentID.F5C_INDEX_FASTA_FILE));
         arguments.add(argument);
-        argument = new Argument(true, "output file", null, "Path to the output sam file", true, "-o", false, true);
+        argument = new Argument(ArgumentID.MINIMAP2_OUTPUT_FILE, true, "output file", null, "minimap2.out.sam",
+                "Path to the output sam file",
+                true, "-o", false,
+                true, new Pair<>(PipelineStep.SAMTOOL_SORT, ArgumentID.SAMTOOL_SORT_INPUT_FILE));
         arguments.add(argument);
         argument = new Argument(false, "Output format SAM", null,
                 "Generate CIGAR and output alignments in SAM ", true, "-a", true);
@@ -86,22 +94,30 @@ public class Step {
     }
 
     private void samtoolSortArguments() {
-        Argument argument = new Argument(true, "input sam", null, "Path to the input sam file", false, null, false,
-                true);
+        Argument argument = new Argument(ArgumentID.SAMTOOL_SORT_INPUT_FILE, true, "input sam", null, null,
+                "Path to the input sam file",
+                false, null, false,
+                true, null);
         arguments.add(argument);
-        argument = new Argument(true, "output file", null, "Path to the output file", true, "-o", false, true);
+        argument = new Argument(ArgumentID.SAMTOOL_SORT_OUTPUT_FILE, true, "output file", null, "samtool.sorted.bam",
+                "Path to the output file",
+                true, "-o", false,
+                true,
+                new Pair<>(PipelineStep.SAMTOOL_INDEX, ArgumentID.SAMTOOL_INDEX_INPUT_FILE));
         arguments.add(argument);
-        argument = new Argument(false, "max memory", "768M", "max memory per thread; suffix K/M/G recognized", true,
-                "-m", false,
-                false);
+        argument = new Argument(false, "max memory", "768M",
+                "max memory per thread; suffix K/M/G recognized",
+                true, "-m", false);
         arguments.add(argument);
         argument = new Argument(false, "threads", null, "Number of threads[1]", true, "-@", false);
         arguments.add(argument);
     }
 
     private void samtoolIndexArguments() {
-        Argument argument = new Argument(true, "input bam", null, "Path to the input bam file", false, null, false,
-                true);
+        Argument argument = new Argument(ArgumentID.SAMTOOL_INDEX_INPUT_FILE, true, "input bam", null, null,
+                "Path to the input bam file",
+                false, null, false,
+                true, new Pair<>(PipelineStep.F5C_CALL_METHYLATION, ArgumentID.F5C_METH_SORTED_FILE));
         arguments.add(argument);
         argument = new Argument(false, "threads", null, "Number of threads[1]", true, "-@", false);
         arguments.add(argument);
@@ -109,20 +125,24 @@ public class Step {
 
     private void f5cIndexArguments() {
         //Build an index mapping from basecalled reads to the signals measured by the sequencer (same as nanopolish index)
-        Argument argument = new Argument(true, "fast5 files", null,
+        Argument argument = new Argument(ArgumentID.F5C_INDEX_FAST5_FILE, true, "fast5 files", null, null,
                 "path to the directory containing the raw ONT signal files. This option can be given multiple times.",
-                true, "--directory", false, true);
+                true, "--directory", false,
+                true, null);
         arguments.add(argument);
-        argument = new Argument(true, "fast(a or q) reads", null,
-                "Path to the reads.fasta or fastq file", false, null, false, true);
+        argument = new Argument(ArgumentID.F5C_INDEX_FASTA_FILE, true, "fast(a or q) reads", null, "fast5_files",
+                "Path to the reads.fasta or fastq file",
+                false, null, false,
+                true, new Pair<>(PipelineStep.F5C_CALL_METHYLATION, ArgumentID.F5C_METH_FASTA_FILE));
         arguments.add(argument);
         argument = new Argument(false, "summary file", null,
                 "the sequencing summary file from albacore, providing this option will make indexing much faster",
                 true, "--sequencing-summary", false);
         arguments.add(argument);
-        argument = new Argument(false, "paths to summary files", null,
+        argument = new Argument(ArgumentID.F5C_INDEX_SUMMARY_FILE, false, "paths to summary files", null, null,
                 "file containing the paths to the sequencing summary files (one per line)",
-                true, "--summary-fofn", false, true);
+                true, "--summary-fofn", false,
+                true, null);
         arguments.add(argument);
         argument = new Argument(false, "verbose", null,
                 "display verbose output",
@@ -133,15 +153,26 @@ public class Step {
 
     private void f5cCallMethylationArguments() {
         //Classify nucleotides as methylated or not (optimised nanopolish call-methylation)
-        Argument argument = new Argument(true, "fastq/fasta read file", null, "fastq/fasta read file", true, "-r",
-                false, true);
+        Argument argument = new Argument(ArgumentID.F5C_METH_FASTA_FILE, true, "fastq/fasta read file", null, null,
+                "fastq/fasta read file",
+                true, "-r", false,
+                true, new Pair<>(PipelineStep.F5C_EVENT_ALIGNMENT, ArgumentID.F5C_ALIGN_FASTA_FILE));
         arguments.add(argument);
-        argument = new Argument(true, "sorted bam file", null, "sorted bam file", true, "-b", false, true);
+        argument = new Argument(ArgumentID.F5C_METH_SORTED_FILE, true, "sorted bam file", null, null,
+                "sorted bam file",
+                true, "-b", false,
+                true, new Pair<>(PipelineStep.F5C_EVENT_ALIGNMENT, ArgumentID.F5C_ALIGN_SORTED_FILE));
         arguments.add(argument);
-        argument = new Argument(true, "reference genome", null, "reference genome", true, "-g", false, true);
+        argument = new Argument(ArgumentID.F5C_METH_REF_FILE, true, "reference genome", null, null,
+                "reference genome",
+                true, "-g", false,
+                true, new Pair<>(PipelineStep.F5C_EVENT_ALIGNMENT, ArgumentID.F5C_ALIGN_REF_FILE));
         arguments.add(argument);
-        argument = new Argument(true, "output tsv file", null, "tsv file from call-methylation", true, "-o", false,
-                true);
+        argument = new Argument(ArgumentID.F5C_METH_OUTPUT_FILE, true, "output tsv file", null,
+                "f5c-call-methylation.tsv",
+                "tsv file from call-methylation",
+                true, "-o", false,
+                true, null);
         arguments.add(argument);
         argument = new Argument(false, "number of threads", null, "number of threads [8]", true, "-t", false);
         arguments.add(argument);
@@ -174,17 +205,25 @@ public class Step {
     private void f5cEventAlignArguments() {
 
         //Align nanopore events to reference k-mers (optimised nanopolish eventalign)
-        Argument argument = new Argument(true, "fastq/fasta read file", null,
-                "fastq/fasta read file", true, "-r", false, true);
+        Argument argument = new Argument(ArgumentID.F5C_ALIGN_FASTA_FILE, true, "fastq/fasta read file", null, null,
+                "fastq/fasta read file",
+                true, "-r", false,
+                true, null);
         arguments.add(argument);
-        argument = new Argument(true, "sorted bam", null,
-                "path to sorted bam file", true, "-b", false, true);
+        argument = new Argument(ArgumentID.F5C_ALIGN_SORTED_FILE, true, "sorted bam file", null, null,
+                "sorted bam file",
+                true, "-b", false,
+                true, null);
         arguments.add(argument);
-        argument = new Argument(true, "reference genome", null,
-                "path to reference genome", true, "-g", false, true);
+        argument = new Argument(ArgumentID.F5C_ALIGN_REF_FILE, true, "reference genome", null, null,
+                "reference genome",
+                true, "-g", false,
+                true, null);
         arguments.add(argument);
-        argument = new Argument(true, "output file", null,
-                "path to output summary", true, "-o", false, true);
+        argument = new Argument(ArgumentID.F5C_ALIGN_OUTPUT_FILE, true, "output file", null, "f5c-event-align.txt",
+                "path to output summary",
+                true, "-o", false,
+                true, null);
         arguments.add(argument);
         argument = new Argument(false, "threads", "8",
                 "Number of threads[8]", true, "-t", false);
@@ -207,5 +246,14 @@ public class Step {
 
     public String getCommandString() {
         return command.toString();
+    }
+
+    public Argument findArgumentById(int argId) {
+        for (Argument argument : arguments) {
+            if (argument.getArgID() == argId) {
+                return argument;
+            }
+        }
+        return null;
     }
 }
