3306ed036144894aa919833cdf952d87bccb7b2f
Sanoj <sanoj.punchihewa@gmail.com>
2019-10-12 20:01:03 +0530

    Add argument ID file

---
+++ app/src/main/java/com/mobilegenomics/f5n/ArgumentID.java
@@ -0,0 +1,39 @@
+package com.mobilegenomics.f5n;
+
+public class ArgumentID {
+
+    public static final int MINIMAP2_REF_FILE = 0;
+
+    public static final int MINIMAP2_QUERY_FILE = 1;
+
+    public static final int MINIMAP2_OUTPUT_FILE = 2;
+
+    public static final int SAMTOOL_SORT_INPUT_FILE = 3;
+
+    public static final int SAMTOOL_SORT_OUTPUT_FILE = 4;
+
+    public static final int SAMTOOL_INDEX_INPUT_FILE = 5;
+
+    public static final int F5C_INDEX_FAST5_FILE = 6;
+
+    public static final int F5C_INDEX_FASTA_FILE = 7;
+
+    public static final int F5C_INDEX_SUMMARY_FILE = 8;
+
+    public static final int F5C_METH_FASTA_FILE = 9;
+
+    public static final int F5C_METH_SORTED_FILE = 10;
+
+    public static final int F5C_METH_REF_FILE = 11;
+
+    public static final int F5C_METH_OUTPUT_FILE = 12;
+
+    public static final int F5C_ALIGN_FASTA_FILE = 13;
+
+    public static final int F5C_ALIGN_SORTED_FILE = 14;
+
+    public static final int F5C_ALIGN_REF_FILE = 15;
+
+    public static final int F5C_ALIGN_OUTPUT_FILE = 16;
+
+}
