715dddfeaa2f86824123c752790ba0d29c1b4039
AnjanaSenanayake <anjanajayabandara@gmail.com>
2019-10-18 20:58:49 +0530

    connectToServer method singularized, code clutter reduced

--- app/src/main/java/com/mobilegenomics/f5n/activity/MinITActivity.java
+++ app/src/main/java/com/mobilegenomics/f5n/activity/MinITActivity.java
@@ -1,8 +1,5 @@
 package com.mobilegenomics.f5n.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -10,40 +7,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.mobilegenomics.f5n.R;
+import com.mobilegenomics.f5n.dto.State;
 import com.mobilegenomics.f5n.support.ServerConnectionUtils;
 
 public class MinITActivity extends AppCompatActivity {
 
     private static TextView connectionLogText;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_minit);
-
-        final EditText serverAddressInput = findViewById(R.id.input_server_address);
-        connectionLogText = findViewById(R.id.text_conn_log);
-        final Button btnConnectServer = findViewById(R.id.btn_connect_server);
-        final Button btnProcessJob = findViewById(R.id.btn_process);
-
-        btnConnectServer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ServerConnectionUtils.setServerAddress(serverAddressInput.getText().toString());
-                ServerConnectionUtils.connectToServer();
-                btnProcessJob.setVisibility(View.VISIBLE);
-            }
-        });
-
-        btnProcessJob.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final ProgressDialog progressDialog = new ProgressDialog(MinITActivity.this);
-                ServerConnectionUtils.sendResult();
-            }
-        });
-    }
 
     public static void logHandler(Handler handler) {
         handler.post(new Runnable() {
@@ -52,6 +24,33 @@ public class MinITActivity extends AppCompatActivity {
                 StringBuilder newLogMessage = ServerConnectionUtils.getLogMessage();
                 if (newLogMessage != null && newLogMessage.toString().trim().length() != 0)
                     connectionLogText.setText(newLogMessage);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minit);
+
+        final EditText serverAddressInput = findViewById(R.id.input_server_address);
+        connectionLogText = findViewById(R.id.text_conn_log);
+        final Button btnRquestJob = findViewById(R.id.btn_request_job);
+        final Button btnSendResult = findViewById(R.id.btn_send_result);
+
+        btnRquestJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerConnectionUtils.setServerAddress(serverAddressInput.getText().toString());
+                ServerConnectionUtils.connectToServer(State.REQUEST);
+                btnSendResult.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnSendResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ServerConnectionUtils.connectToServer(State.COMPLETED);
             }
         });
     }
--- app/src/main/java/com/mobilegenomics/f5n/support/ServerConnectionUtils.java
+++ app/src/main/java/com/mobilegenomics/f5n/support/ServerConnectionUtils.java
@@ -1,10 +1,7 @@
 package com.mobilegenomics.f5n.support;
 
-import android.app.ProgressDialog;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 import com.mobilegenomics.f5n.activity.MinITActivity;
 import com.mobilegenomics.f5n.dto.State;
@@ -18,33 +15,11 @@ import java.net.Socket;
 public class ServerConnectionUtils {
 
     private final static String TAG = "ServerConnectionUtils";
-
-    enum ConnectionMessages{
-        CONN_SUCCESSS("Connection to the server is successful"),
-        CONN_FAILED("Connection to the server is failed. Trying again..."),
-        RECONN_SUCCESS("Re-connection to the server is successful"),
-        RECONN_FAILED("Re-connection to the server is failed. Trying again..."),
-        JOB_RECV_SUCCESS("Job received successfully"),
-        TO_SERVER("To server"),
-        FROM_SERVER("From server"),
-        NONE("None");
-
-        String message;
-
-        ConnectionMessages(String msg) {
-            message = msg;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
-
     private static String serverAddress;
     private static StringBuilder logMessage = new StringBuilder();
     private static WrapperObject receivedWrapperObject;
 
-    public static void connectToServer() {
+    public static void connectToServer(final State state) {
         final Handler handler = new Handler();
         new Thread(new Runnable() {
             @Override
@@ -54,7 +29,7 @@ public class ServerConnectionUtils {
                     Socket socket = new Socket(serverAddress.trim(), 6677);
                     ObjectOutputStream objectOutStream = new ObjectOutputStream(socket.getOutputStream());
                     WrapperObject initialMessage = new WrapperObject();
-                    initialMessage.setState(State.CONNECT);
+                    initialMessage.setState(state);
                     objectOutStream.writeObject(initialMessage);
                     objectOutStream.flush();
                     Log.d(TAG, connectionLog(ConnectionMessages.TO_SERVER, initialMessage.toString()));
@@ -63,68 +38,35 @@ public class ServerConnectionUtils {
                     WrapperObject receivedObjectMessage = (WrapperObject) objectInStream.readObject();
                     Log.d(TAG, connectionLog(ConnectionMessages.FROM_SERVER, receivedObjectMessage.toString()));
 
-                    if (receivedObjectMessage.getState().equals(State.ACK)) {
-                        //isConnected = true;
-                        Log.d(TAG,connectionLog(ConnectionMessages.NONE, ConnectionMessages.CONN_SUCCESSS.getMessage()));
-                        WrapperObject receivedJobMessage = (WrapperObject) objectInStream.readObject();
-                        receivedWrapperObject = receivedJobMessage;
-                        Log.d(TAG,connectionLog(ConnectionMessages.NONE, ConnectionMessages.JOB_RECV_SUCCESS.getMessage()));
-                        Log.d(TAG,connectionLog(ConnectionMessages.FROM_SERVER, receivedJobMessage.toStringPretty()));
-                    } else {
-                        //isConnected = false;
-                        Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.CONN_FAILED.getMessage()));
+                    if (state == State.REQUEST) {
+                        if (receivedObjectMessage.getState().equals(State.ACK)) {
+                            //isConnected = true;
+                            Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.CONN_SUCCESSS.getMessage()));
+                            WrapperObject receivedJobMessage = (WrapperObject) objectInStream.readObject();
+                            receivedWrapperObject = receivedJobMessage;
+                            Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.JOB_RECV_SUCCESS.getMessage()));
+                            Log.d(TAG, connectionLog(ConnectionMessages.FROM_SERVER, receivedJobMessage.toStringPretty()));
+                        } else {
+                            //isConnected = false;
+                            Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.CONN_FAILED.getMessage()));
+                        }
+                    } else if (state == State.COMPLETED) {
+                        if (receivedObjectMessage.getState().equals(State.ACK)) {
+                            //isConnected = true;
+                            Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.RECONN_SUCCESS.getMessage()));
+                            receivedWrapperObject.setState(State.SUCCESS);
+                            objectOutStream.writeObject(receivedWrapperObject);
+                            Log.d(TAG, connectionLog(ConnectionMessages.TO_SERVER, receivedWrapperObject.toStringPretty()));
+                        } else {
+                            //isConnected = false;
+                            Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.RECONN_FAILED.getMessage()));
+                        }
                     }
                     objectInStream.close();
                     objectOutStream.close();
                     socket.close();
-
-                    //process message
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                    Log.d(TAG, connectionLog(ConnectionMessages.NONE, ConnectionMessages.CONN_CLOSED.getMessage()));
                     MinITActivity.logHandler(handler);
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-    }
-
-    public static void sendResult() {
-        final Handler handler = new Handler();
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Socket socket = new Socket(serverAddress.trim(), 6677);
-
-                    ObjectOutputStream objectOutStream = new ObjectOutputStream(socket.getOutputStream());
-                    WrapperObject initialMessage = new WrapperObject();
-                    initialMessage.setState(State.SUCCESS);
-                    objectOutStream.writeObject(initialMessage);
-                    objectOutStream.flush();
-                    Log.d(TAG, connectionLog(ConnectionMessages.TO_SERVER, initialMessage.toString()));
-
-                    ObjectInputStream objectInStream = new ObjectInputStream(socket.getInputStream());
-                    WrapperObject receivedObjectMessage = (WrapperObject) objectInStream.readObject();
-                    Log.d(TAG,connectionLog(ConnectionMessages.FROM_SERVER, receivedObjectMessage.toString()));
-
-                    if (receivedObjectMessage.getState().equals(State.ACK)) {
-                        //isConnected = true;
-                        Log.d(TAG,connectionLog(ConnectionMessages.NONE, ConnectionMessages.RECONN_SUCCESS.getMessage()));
-
-                        receivedWrapperObject.setState(State.SUCCESS);
-                        objectOutStream.writeObject(receivedWrapperObject);
-                        Log.d(TAG,connectionLog(ConnectionMessages.TO_SERVER, receivedWrapperObject.toStringPretty()));
-                    } else {
-                        //isConnected = false;
-                        Log.d(TAG,connectionLog(ConnectionMessages.NONE, ConnectionMessages.RECONN_FAILED.getMessage()));
-                    }
-                    MinITActivity.logHandler(handler);
-                    objectInStream.close();
-                    objectOutStream.close();
-                    socket.close();
                 } catch (IOException | ClassNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -134,10 +76,10 @@ public class ServerConnectionUtils {
 
     private static String connectionLog(ConnectionMessages connectionMessages, String logInstance) {
         String strInstance;
-        if(connectionMessages == ConnectionMessages.TO_SERVER) {
+        if (connectionMessages == ConnectionMessages.TO_SERVER) {
             strInstance = "To server: " + logInstance;
             logMessage.append("\n").append(strInstance);
-        } else if(connectionMessages == ConnectionMessages.FROM_SERVER) {
+        } else if (connectionMessages == ConnectionMessages.FROM_SERVER) {
             strInstance = "From server: " + logInstance;
             logMessage.append("\n").append(strInstance);
         } else {
@@ -153,5 +95,27 @@ public class ServerConnectionUtils {
 
     public static StringBuilder getLogMessage() {
         return logMessage;
+    }
+
+    enum ConnectionMessages {
+        CONN_SUCCESSS("Connection to the server is successful"),
+        CONN_FAILED("Connection to the server is failed. Try again"),
+        RECONN_SUCCESS("Re-connection to the server is successful"),
+        RECONN_FAILED("Re-connection to the server is failed. Try again"),
+        JOB_RECV_SUCCESS("Job received successfully"),
+        CONN_CLOSED("Server connection closed"),
+        TO_SERVER("To server"),
+        FROM_SERVER("From server"),
+        NONE("None");
+
+        String message;
+
+        ConnectionMessages(String msg) {
+            message = msg;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
