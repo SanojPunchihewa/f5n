4badefccfafac2819a27fd8d2a7eebee60ee69ae
Sanoj <sanoj.punchihewa@gmail.com>
2019-10-19 21:11:56 +0530

    Move TimeFormat helper class to support package

--- app/src/main/java/com/mobilegenomics/f5n/GUIConfiguration.java
+++ app/src/main/java/com/mobilegenomics/f5n/GUIConfiguration.java
@@ -2,12 +2,10 @@ package com.mobilegenomics.f5n;
 
 import android.content.Context;
 import android.util.Log;
-
 import com.mobilegenomics.f5n.core.PipelineComponent;
 import com.mobilegenomics.f5n.core.PipelineStep;
 import com.mobilegenomics.f5n.core.Step;
-import com.mobilegenomics.f5n.core.TimeFormat;
-
+import com.mobilegenomics.f5n.support.TimeFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
