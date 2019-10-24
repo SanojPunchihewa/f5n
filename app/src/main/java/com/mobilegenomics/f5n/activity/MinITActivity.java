fc1cdb54e3dcf41f2058becc27fd27fd319b98f4
Sanoj <sanoj.punchihewa@gmail.com>
2019-10-24 11:23:04 +0530

    Update send Job Results

--- app/src/main/java/com/mobilegenomics/f5n/activity/MinITActivity.java
+++ app/src/main/java/com/mobilegenomics/f5n/activity/MinITActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -102,6 +103,8 @@ public class MinITActivity extends AppCompatActivity {
                         GUIConfiguration.configureSteps(job.getSteps());
                         zipFileName = job.getPrefix();
                         btnSendResult.setVisibility(View.VISIBLE);
+                        Log.d("TAG", "Prefix = " + job.getPrefix());
+                        Log.d("TAG", "Dir Path = " + job.getPathToDataDir());
                     }
                 });
             }
@@ -114,11 +117,11 @@ public class MinITActivity extends AppCompatActivity {
     }
 
     private void sendJobResults() {
-        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
-        ServerConnectionUtils.connectToServer(State.SUCCESS, new ServerCallback() {
+
+        ServerConnectionUtils.connectToServer(State.COMPLETED, new ServerCallback() {
             @Override
             public void onSuccess(final WrapperObject job) {
-
+                Toast.makeText(MinITActivity.this, "Success", Toast.LENGTH_SHORT).show();
             }
 
             @Override
