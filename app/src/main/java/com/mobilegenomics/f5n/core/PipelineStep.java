7280a17265b2695ba81f41ebc133739d78811e6f
Sanoj <sanoj.punchihewa@gmail.com>
2019-11-10 21:32:57 +0530

    Update pipeline step

--- app/src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
+++ app/src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
@@ -1,13 +1,11 @@
 package com.mobilegenomics.f5n.core;
 
 public enum PipelineStep {
-    //    MINIMAP2_SEQUENCE_ALIGNMENT(0,
-//        "minimap2 -x map-ont -a /mnt/sdcard/f5c/test/ecoli_2kb_region/draft.fa /mnt/sdcard/f5c/test/ecoli_2kb_region/reads.fasta"),
-    MINIMAP2_SEQUENCE_ALIGNMENT(0, "minimap2 -ax map-ont"),
+    MINIMAP2_SEQUENCE_ALIGNMENT(0, "minimap2 -x map-ont"),
     SAMTOOL_SORT(1, "samtool sort"),
     SAMTOOL_INDEX(2, "samtool index"),
     F5C_INDEX(3, "f5c index"),
-    F5C_CALL_METHYLATION(4, "f5c call-methylation --secondary=yes --min-mapq=0 -B 2M"),
+    F5C_CALL_METHYLATION(4, "f5c call-methylation"),
     F5C_EVENT_ALIGNMENT(5, "f5c eventalign");
 
     private final int value;
