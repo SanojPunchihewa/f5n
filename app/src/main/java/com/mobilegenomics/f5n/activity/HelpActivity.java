7691a895c655749eca0163de965f0e95e63dcceb
Sanoj <sanoj.punchihewa@gmail.com>
2020-01-17 16:08:28 +0530

    Update Help window

--- app/src/main/java/com/mobilegenomics/f5n/activity/HelpActivity.java
+++ app/src/main/java/com/mobilegenomics/f5n/activity/HelpActivity.java
@@ -1,8 +1,10 @@
 package com.mobilegenomics.f5n.activity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,10 +25,37 @@ public class HelpActivity extends AppCompatActivity {
 
         linearLayout = findViewById(R.id.vertical_linear_layout);
 
-        TextView txtIntro = new TextView(this);
-        txtIntro.setText(
-                getResources().getString(R.string.app_info));
-        linearLayout.addView(txtIntro);
+        TextView txtIntro1 = new TextView(this);
+        txtIntro1.setText(
+                "F5N is a Mobile application for Reference Guided Sequence Alignment using Oxford Nanopore Technology Data\n");
+        txtIntro1.setTypeface(null, Typeface.BOLD);
+        linearLayout.addView(txtIntro1);
+
+        TextView txtIntro2 = new TextView(this);
+        txtIntro2.setText(
+                "Please read the following terms before you use this application\n");
+        linearLayout.addView(txtIntro2);
+
+        TextView txtIntro3 = new TextView(this);
+        txtIntro3.setText(
+                "This app needs <b>storage permission</b> in order to read/write files generated from the pipeline steps\n");
+        linearLayout.addView(txtIntro3);
+
+        TextView txtIntro4 = new TextView(this);
+        txtIntro4.setText(Html.fromHtml("This app needs at least <b>2GB free RAM</b> to smoothly function<br>"));
+        linearLayout.addView(txtIntro4);
+
+        TextView txtIntro5 = new TextView(this);
+        txtIntro5.setText(Html.fromHtml(
+                "If you have less memory available, minimizing the app while running some process, may stop the "
+                        + "process and generate incomplete results<br>"));
+        linearLayout.addView(txtIntro5);
+
+        TextView txtIntro6 = new TextView(this);
+        txtIntro6.setText(
+                Html.fromHtml("Refer <b>f5n.log</b> in main_storage/mobile-genomics folder to read the log.<br>"
+                        + "Refer <b>tmp.log</b> in main_storage/mobile-genomics folder in cases where the app fails in the middle of a process.<br>"));
+        linearLayout.addView(txtIntro6);
 
         Button btnTutorial = new Button(this);
         btnTutorial.setText("View Tutorial");
@@ -40,7 +69,7 @@ public class HelpActivity extends AppCompatActivity {
 
         TextView txtContribute = new TextView(this);
         txtContribute.setText(
-                "Please contribute to our work by testing,debugging, developing and submitting issues on our product");
+                "\nPlease contribute to our work by testing,debugging, developing and submitting issues on our product\n");
         linearLayout.addView(txtContribute);
 
         Button btnReport = new Button(this);
