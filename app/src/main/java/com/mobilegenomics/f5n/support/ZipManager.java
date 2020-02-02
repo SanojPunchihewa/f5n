6c38b65da8338bc1da7978dd36d60281ce2a2ddc
Sanoj <sanoj.punchihewa@gmail.com>
2020-02-02 10:40:05 +0530

    Update ZipManager to zip list of files

--- app/src/main/java/com/mobilegenomics/f5n/support/ZipManager.java
+++ app/src/main/java/com/mobilegenomics/f5n/support/ZipManager.java
@@ -9,15 +9,16 @@ import android.util.Log;
 import androidx.documentfile.provider.DocumentFile;
 import com.mobilegenomics.f5n.R;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
@@ -281,41 +282,47 @@ public class ZipManager {
         new UnzipAysnc().execute();
     }
 
-    public void zip(String sourcePath) {
+    public void zip(ArrayList<String> files, String zipFileName) {
 
         class ZipAysnc extends AsyncTask<String, Integer, Boolean> {
 
-            private String zipPath;
-
-            FileOutputStream fos;
-
-            ZipOutputStream zos;
-
-            File sourceFile;
+            int BUFFER = 1024;
 
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
-                sourceFile = new File(sourcePath);
-                totalBytesToCompress = getDirectorySize(sourceFile);
+                totalBytesToCompress = getFileListSize(files);
                 zipListener.onStarted(totalBytesToCompress);
                 Log.i(TAG, "Starting to zip");
-                zipPath = sourcePath + ".out.zip";
-                try {
-                    fos = new FileOutputStream(zipPath);
-                } catch (FileNotFoundException e) {
-                    Log.e(TAG, "File IO Error: " + e);
-                }
-                zos = new ZipOutputStream(fos);
-
             }
 
             @Override
             protected Boolean doInBackground(final String... strings) {
 
-                File sourceFile = new File(sourcePath);
                 try {
-                    addDirToZipArchive(zos, sourceFile, null);
+                    BufferedInputStream origin = null;
+                    FileOutputStream dest = new FileOutputStream(zipFileName);
+                    ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
+                    byte data[] = new byte[BUFFER];
+
+                    for (int i = 0; i < files.size(); i++) {
+                        Log.v("Compress", "Adding: " + files.get(i));
+                        FileInputStream fi = new FileInputStream(files.get(i));
+                        origin = new BufferedInputStream(fi, BUFFER);
+
+                        ZipEntry entry = new ZipEntry(files.get(i).substring(files.get(i).lastIndexOf("/") + 1));
+                        out.putNextEntry(entry);
+                        int count;
+
+                        while ((count = origin.read(data, 0, BUFFER)) != -1) {
+                            out.write(data, 0, count);
+                            compressedBytes += count;
+                            zipListener.onProgress(compressedBytes, totalBytesToCompress);
+                        }
+                        origin.close();
+                    }
+
+                    out.close();
                 } catch (Exception e) {
                     Log.e(TAG, "Zip Exception: " + e);
                     return false;
@@ -333,47 +340,9 @@ public class ZipManager {
                 super.onPostExecute(result);
                 Log.i(TAG, "Finished zip");
                 zipListener.onComplete(result, null);
-                try {
-                    zos.flush();
-                    fos.flush();
-                    zos.close();
-                    fos.close();
-                } catch (IOException e) {
-                    Log.e(TAG, "File IO Error: " + e);
-                }
             }
         }
         new ZipAysnc().execute();
-    }
-
-    public void addDirToZipArchive(ZipOutputStream zos, File fileToZip, String parentDirectoryName)
-            throws Exception {
-        if (fileToZip == null || !fileToZip.exists()) {
-            return;
-        }
-
-        String zipEntryName = fileToZip.getName();
-        if (parentDirectoryName != null && !parentDirectoryName.isEmpty()) {
-            zipEntryName = parentDirectoryName + "/" + fileToZip.getName();
-        }
-
-        if (fileToZip.isDirectory()) {
-            for (File file : fileToZip.listFiles()) {
-                addDirToZipArchive(zos, file, zipEntryName);
-            }
-        } else {
-            byte[] buffer = new byte[1024];
-            FileInputStream fis = new FileInputStream(fileToZip);
-            zos.putNextEntry(new ZipEntry(zipEntryName));
-            int length;
-            while ((length = fis.read(buffer)) > 0) {
-                zos.write(buffer, 0, length);
-                compressedBytes += length;
-                zipListener.onProgress(compressedBytes, totalBytesToCompress);
-            }
-            zos.closeEntry();
-            fis.close();
-        }
     }
 
     private void _dirChecker(String dir) {
@@ -387,23 +356,13 @@ public class ZipManager {
         return (int) ((100 * currentBytes) / totalBytes);
     }
 
-    private static long getDirectorySize(File dir) {
-
-        if (dir.exists()) {
-            long result = 0;
-            File[] fileList = dir.listFiles();
-            for (int i = 0; i < fileList.length; i++) {
-                // Recursive call if it's a directory
-                if (fileList[i].isDirectory()) {
-                    result += getDirectorySize(fileList[i]);
-                } else {
-                    // Sum the file size in bytes
-                    result += fileList[i].length();
-                }
-            }
-            return result; // return the file size
+    private static long getFileListSize(ArrayList<String> files) {
+        long totalSize = 0;
+        for (String fileName : files) {
+            File file = new File(fileName);
+            totalSize += file.length();
         }
-        return 0;
+        return totalSize;
     }
 
 }
