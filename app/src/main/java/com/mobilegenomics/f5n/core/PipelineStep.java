960e1a780fbbbcaa1ed69b1f28ea45c3de41c023
SanojPunchihewa <sanoj.punchihewa@gmail.com>
2020-04-21 13:36:16 +0530

    Add bcftools steps to variant pipeline

--- app/src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
+++ app/src/main/java/com/mobilegenomics/f5n/core/PipelineStep.java
@@ -34,6 +34,16 @@ public class PipelineStep implements EnumHelper {
 
     public static final int ARTIC_TRIM = 9;
 
+    public static final int BCFTOOLS_CONSENSUS = 10;
+
+    public static final int BCFTOOLS_CONCAT = 11;
+
+    public static final int BCFTOOLS_REHEADER = 12;
+
+    public static final int BCFTOOLS_VIEW = 13;
+
+    public static final int BCFTOOLS_INDEX = 14;
+
     private final int value;
 
     private final String name;
