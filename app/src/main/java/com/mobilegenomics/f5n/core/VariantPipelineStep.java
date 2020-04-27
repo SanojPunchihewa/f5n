9fa81d09868ee2cef3a486b539c752298ce5dbb6
SanojPunchihewa <sanoj.punchihewa@gmail.com>
2020-04-27 19:05:39 +0530

    Revert "Create Artic Pipeline Steps"
    This reverts commit 6aa82a62

--- app/src/main/java/com/mobilegenomics/f5n/core/VariantPipelineStep.java
+++ app/src/main/java/com/mobilegenomics/f5n/core/VariantPipelineStep.java
@@ -10,6 +10,9 @@ public class VariantPipelineStep extends PipelineStep {
     private static final PipelineStep nanopolishVariant = new PipelineStep(NANOPOLISH_VARIANT, "NANOPOLISH_VARIANT",
             "nanopolish variants");
 
+    private static final PipelineStep articTrim = new PipelineStep(ARTIC_TRIM, "ARTIC_TRIM",
+            "artic");
+
     public VariantPipelineStep() {
 
     }
@@ -17,7 +20,7 @@ public class VariantPipelineStep extends PipelineStep {
     @Override
     public PipelineStep[] values() {
         PipelineStep[] common = super.values();
-        PipelineStep[] nanopolishSteps = new PipelineStep[]{nanopolishIndex, nanopolishVariant};
+        PipelineStep[] nanopolishSteps = new PipelineStep[]{nanopolishIndex, nanopolishVariant, articTrim};
         PipelineStep[] merged = Arrays.copyOf(common, common.length + nanopolishSteps.length);
         System.arraycopy(nanopolishSteps, 0, merged, common.length, nanopolishSteps.length);
         return merged;
