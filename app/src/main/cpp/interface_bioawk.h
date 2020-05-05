2eeac32f7434639e02f737cdd2cc5dec2751c2a4
SanojPunchihewa <sanoj.punchihewa@gmail.com>
2020-05-05 13:36:30 +0530

    Add artic, bcftool and bioawk native lib

---
+++ app/src/main/cpp/interface_bioawk.h
@@ -0,0 +1,12 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifndef INTERFACE_BIOAWK_H
+#define INTERFACE_BIOAWK_H
+
+int init_bioawk(int argc, char *argv[]);
+
+#endif
+#ifdef __cplusplus
+}
+#endif
