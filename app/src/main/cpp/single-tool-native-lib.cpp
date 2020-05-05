507f5162101065349d48c3d1c26609e2d749d289
hiruna7 <h.samarakoon@20face.com>
2020-05-06 01:14:55 +0530

    arguments are parsed cosidering single quoted arguments, specially in case where expressions are passed,e.g.,bioawk

--- app/src/main/cpp/single-tool-native-lib.cpp
+++ app/src/main/cpp/single-tool-native-lib.cpp
@@ -82,19 +82,56 @@ Java_com_mobilegenomics_f5n_core_NativeCommands_init(JNIEnv *env, jobject, jstri
   // Convert command to cpp
   //TODO:casting not good
   char *command_c = (char *) env->GetStringUTFChars(command, nullptr);
-
+  int max_len = strlen(command_c);
   enum { kMaxArgs = 64 };
   int argc = 0;
   char *argv[kMaxArgs];
 
   char *p2 = strtok(command_c, " ");
 
-  while (p2 && argc < kMaxArgs - 1) {
-    argv[argc++] = p2;
+  const char search = '\'';
+
+  while (p2 && argc < kMaxArgs-1){ // arguments can be lengthy, quoted ones. Hence, have to do more parsing than in other pipelines.
+    // fprintf(stderr,"p = %s\n",p2);
+    char * pos1 = strchr(p2,search);
+    char * argument_ptr = (char*)malloc(max_len);
+    strcpy(argument_ptr, p2);
+    int has_quotes = 0;
+    if(pos1 != NULL){
+        char * pos2 = strchr(pos1+1,search);
+        if(pos2 == NULL){
+            char * pos3;
+            do {
+                p2 = strtok(0, " ");
+                strcat(argument_ptr, " ");
+                strcat(argument_ptr, p2);
+                pos3 = strchr(p2,search);
+            }while (p2 && pos3 == NULL);
+            if(*(pos3+1) != '\0'){
+                fprintf(stderr,"%c is not found at the end of an argument",search);
+                return 1;
+            }
+        }
+        else if(*(pos2+1) != '\0'){
+            fprintf(stderr,"%c is found more than twice in an argument.\n",search);
+            return 1;
+        }
+        // strip ['] from argument
+        has_quotes = 1;
+        // argument_ptr++;
+        argument_ptr[strlen(argument_ptr)-1] = 0;
+
+    }
+    if(has_quotes){
+        argv[argc++] = ++argument_ptr;
+    } else{
+        argv[argc++] = argument_ptr;
+    }
     p2 = strtok(0, " ");
-  }
+    }
+
   argv[argc] = 0;
-  jint result;
+  jint result = 1;
   char exceptionBuffer[1024];
   // http://jnicookbook.owsiak.org/recipe-no-016/
   atexit(stopExit);
@@ -140,6 +177,10 @@ Java_com_mobilegenomics_f5n_core_NativeCommands_init(JNIEnv *env, jobject, jstri
 
   env->ReleaseStringUTFChars(command, command_c);
 
+  // releasing dynamically allocated argvs
+  for(int i = 0; i<argc;i++){
+    free(argv[i]);
+  }
   return result;
 }
 
